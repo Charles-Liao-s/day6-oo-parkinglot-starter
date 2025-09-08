97369ff934e185828d4ecbf62ab143e37d448e75
Charles Liao <charles.liao@oocl.com>
2025-09-08 16:08:15 +0800

    feat: implement ParkingLot and Car classes with basic parking functionality

--- src/main/java/com/afs/parkinglot/ParkingLot.java
+++ src/main/java/com/afs/parkinglot/ParkingLot.java
@@ -1,4 +1,15 @@
 package com.afs.parkinglot;
 
+import java.util.List;
+
 public class ParkingLot {
+    private List<Ticket> tickets;
+
+    public Car fetch(Ticket invalidTicket) {
+        return null;
+    }
+
+    public Ticket park(Car car) {
+        return new Ticket();
+    }
 }
--- src/test/java/com/afs/parkinglot/ParkingLotTest.java
+++ src/test/java/com/afs/parkinglot/ParkingLotTest.java
@@ -1,4 +1,48 @@
 package com.afs.parkinglot;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ParkingLotTest {
+
+
+
+    //Case 1 - Given a parking lot, and a car, When park the car, Then return a parking ticket.
+    @Test
+    void should_park_car_and_return_ticket_when_park() {
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car("A234123");
+        Ticket ticket = parkingLot.park(car);
+        assertNotNull(ticket);
+    }
+    //Case 2 - Given a parking lot with a parked car, and a parking ticket, When fetch the car, Then return the parked car.
+//    @Test
+//    void should_return_parked_car_when_fetch_with_valid_ticket() {
+//        ParkingLot parkingLot = new ParkingLot();
+//        Car car = new Car("A234123");
+//        Ticket ticket =new Ticket();
+//        parkingLot.fetch(ticket);
+//        Car fetchedCar = parkingLot.fetch(ticket);
+//        assertEquals(car, fetchedCar);
+//    }
+    //Case 3 - Given a parking lot with two parked cars, and two parking tickets, When fetch the car twice, Then return the right car with each ticket
+//    @Test
+//    void should_return_right_car_when_fetch_twice_with_two_tickets() {
+//        ParkingLot parkingLot = new ParkingLot();
+//        Car car1 = new Car("A234123");
+//        Car car2 = new Car("B234123");
+//        Ticket ticket1 = parkingLot.park(car1);
+//        Ticket ticket2 = parkingLot.park(car2);
+//        Car fetchedCar1 = parkingLot.fetch(ticket1);
+//        Car fetchedCar2 = parkingLot.fetch(ticket2);
+//        assertEquals(car1, fetchedCar1);
+//        assertEquals(car2, fetchedCar2);
+//    }
+    //Case 4 - Given a parking lot, and a wrong parking ticket, When fetch the car, Then return nothing.
+    //Case 5 - Given a parking lot, and a used parking ticket, When fetch the car, Then return nothing.
+    //Case 6 - Given a parking lot without any position, and a car, When park the car, Then return nothing
+
+
+
 }
